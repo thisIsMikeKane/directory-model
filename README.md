93f4dff5f71f5176cbc9fca08a9de285333e75b2
thisismikekane <thisismikekane@gmail.com>
2024-10-18 14:55:01 -0400

    docs and organization

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ directory_model is a Python package for both modeling and validating filesystem 
 
 ```bash
 pip install directory_model
+```
+
+## `directory.yml`
+
+The `directory.yml` file contains the schema that the contents of your directory will be validated against. The `directory.yml` should contain a top level `path:` and may contain a top level `config:`.
+
+`path:`
+
+`config:`
--- src/directory_model/directory_model.py
+++ src/directory_model/directory_model.py
@@ -11,14 +11,25 @@ from directory_model.path_model import PathModel
 
 class DirectoryModel(BaseModel):
     """
-    Define a model for the overall directory tree.
+    DirectoryModel: A model representing the overall directory tree structure. This model includes
+    a list of path objects and configuration settings for validation.
+    
+    Attributes:
+        paths (List[PathModel]): A list of path objects representing each path in the base directory.
+        config (ConfigModel): Configuration settings used for validation.
+    
+    Methods:
+        validate_project_structure() -> DirectoryModel:
+            Validates the project structure by ensuring each path object conforms to the given configuration.
+        print_tree():
+            Prints the directory tree structure to the console using a tree representation.
     """
 
     paths: List[PathModel] = Field(
         ...,
         description="List of each path object in the base directory."
     )
-    
+
     config: ConfigModel = Field(
         default_factory=ConfigModel,
         description="Configuration settings for validation"
